f5aa9c858fdb47c133cddf3022ded41864bb21e0
Andrey Xezzus <xezzus@gmail.com>
2016-06-19 11:54:29 +0300

    Developing

--- Core.php
+++ Core.php
@@ -107,6 +107,7 @@ class Core {
       '<head>'.
       '<title></title>'.
       '<meta charset="utf-8">'.
+      '<meta name="viewport" content="width=device-width, initial-scale=1">'.
       '<link rel="stylesheet" type="text/css" href="/style.css">'.
       '<script src="/script.js" type="text/javascript"></script>'.
       '</head>'.
