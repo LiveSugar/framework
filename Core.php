1ffa541e332001c9aad11b4426c02153409772f8
Andrey Buturlakin <xezzus@gmail.com>
2017-02-18 15:11:10 +0300

    Fix

--- Core.php
+++ Core.php
@@ -208,10 +208,8 @@ class Core {
       '<meta charset="utf-8">'.
       '<meta name="viewport" content="width=device-width, initial-scale=1">'.
       '<style type="text/css">'.$contentCss.'</style>'.
-      //'<link rel="stylesheet" type="text/css" href="/css?path='.Path::$path.'">'.
-      //'<script src="/js?path='.Path::$path.'" type="text/javascript"></script>'.
       '</head>'.
-      '<body><script type="text/javascript">'.$contentJs.'</script>'.$content.'</body></html>';
+      '<body>'.$content.'<script type="text/javascript">'.$contentJs.'</script></body></html>';
 
     $content = preg_replace('/\>\s+\</Uui','><',$content);
     $content = preg_replace('/\s/Uui',' ',$content);
