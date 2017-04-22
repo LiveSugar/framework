cb725311ab34117c1963f349b52c5c1b13540eb8
Andrey Buturlakin <xezzus@gmail.com>
2017-04-22 14:51:17 +0300

    fix

--- Apps.php
+++ Apps.php
@@ -10,11 +10,13 @@ class Apps {
     private static $once = false;
     private static $file = false;
     private static $apis = false;
+    private static $join = false;
 
 
     public function __construct(){
       self::$file = new File;
       self::$apis = new Apis;
+      self::$join = new Join;
     }
 
     public function __get($name){
