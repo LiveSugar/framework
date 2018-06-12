d5a5d22acff39141e0376d67c0b3cf7582c24ba7
xezzus <xezzus@gmail.com>
2018-06-12 14:21:29 +0300

    12-06-2018 14:21:29 development

--- apps/db/pg/pg.php
+++ apps/db/pg/pg.php
@@ -1,5 +1,6 @@
 <?php
 return function($host,$base,$user,$pass) {
+  static $singleton = true;
   $db = new PDO('pgsql:host='.$host.';dbname='.$base.'',$user,$pass,[PDO::ATTR_PERSISTENT => true]);
   $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   return $db;
